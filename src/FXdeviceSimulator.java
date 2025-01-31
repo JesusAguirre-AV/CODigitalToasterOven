4f70ff0a7aa96b16cd2308f4d1458fe038a4ec17
mdetloff1 <martin.detloff@gmail.com>
2025-01-31 15:16:57 -0700

    made a rectangle

--- src/FXdeviceSimulator.java
+++ src/FXdeviceSimulator.java
@@ -1,12 +1,11 @@
 
-    
- 
     import javafx.application.Application;
     import javafx.event.ActionEvent;
     import javafx.event.EventHandler;
     import javafx.scene.Scene;
     import javafx.scene.control.Button;
     import javafx.scene.layout.StackPane;
+    import javafx.scene.shape.Rectangle;
     import javafx.stage.Stage;
      
     public class FXdeviceSimulator extends Application {
@@ -16,19 +15,22 @@
         
         @Override
         public void start(Stage primaryStage) {
-            primaryStage.setTitle("Hello World!");
-            Button btn = new Button();
-            btn.setText("Say 'Hello World'");
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-     
-                @Override
-                public void handle(ActionEvent event) {
-                    System.out.println("Hello World!");
-                }
-            });
+            primaryStage.setTitle("Simulator");
+//            Button btn = new Button();
+//            btn.setText("Say 'Hello World'");
+//            btn.setOnAction(new EventHandler<ActionEvent>() {
+//
+//                @Override
+//                public void handle(ActionEvent event) {
+//                    System.out.println("Hello World!");
+//                }
+//            });
             
             StackPane root = new StackPane();
-            root.getChildren().add(btn);
+//            root.getChildren().add(btn);
+
+            Rectangle rectangle = new Rectangle(10 , 10);
+            root.getChildren().add(rectangle);
             primaryStage.setScene(new Scene(root, 300, 250));
             primaryStage.show();
         }
